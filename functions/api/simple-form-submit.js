34ba45205aa738860dbe6d286ccf282f39a2c7fd
Ric Gordon <digital.plumbertotherescue@gmail.com>
2026-04-25 14:58:31 -0400

    change sender email to service@plumberandelectrician.com.au

--- functions/api/simple-form-submit.js
+++ functions/api/simple-form-submit.js
@@ -88,7 +88,7 @@ GCLID: ${gclid}
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       api_key:   env.SMTP2GO_API_KEY,
-      sender:    "gordo@mrwasher.com.au",
+      sender:    "service@plumberandelectrician.com.au",
       to:        ["jobs@mrwasher.com.au"],
       subject,
       text_body: textBody,
